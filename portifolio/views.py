c04d083d6710017a02091b0647d554224e064006
Felipe <felipeguimaraes9787@gmail.com>
2023-06-08 02:35:12 +0100

    {% block link %}     {% load static %}     <link rel="stylesheet" href="{% static 'portfolio/css/projetos.css' %}"> {% endblock %}

--- portifolio/views.py
+++ portifolio/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
-	return render(request, 'portfolio/home.html')
+    context = {
+        'nome': 'FELIPE'
+    }
+
+    return render(request, 'portfolio/home.html')
+
 
 def index(request):
-	return render(request, 'portfolio/index.html')
+    return render(request, 'portfolio/index.html')
+
 
 def sobreMim(request):
-	return render(request, 'portfolio/sobreMim.html')
+    return render(request, 'portfolio/sobreMim.html')
+
 
 def projetos(request):
-	return render(request, 'portfolio/projetos.html')
+    return render(request, 'portfolio/projetos.html')
+
 
 def contacto(request):
-	return render(request, 'portfolio/contacto.html')
+    return render(request, 'portfolio/contacto.html')
