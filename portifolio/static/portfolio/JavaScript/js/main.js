96049d8b6b8fb369c56157e8db296d0a536dc25e
Felipe <felipeguimaraes9787@gmail.com>
2023-10-14 01:54:18 +0100

    Automate Words

--- portifolio/static/portfolio/JavaScript/js/main.js
+++ portifolio/static/portfolio/JavaScript/js/main.js
@@ -34,7 +34,7 @@
 
 
     // Typed Initiate
-    if ($('.typed-text-output').length == 1) {
+    if ($('.typed-text-output').length === 1) {
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
             strings: typed_strings.split(', '),
