a3eed450a7ad22f2a70799cab5727e0aa4f95fda
Felipe <felipeguimaraes9787@gmail.com>
2023-10-14 15:01:55 +0100

    Add Subjects

--- portifolio/static/portfolio/JavaScript/js/main.js
+++ portifolio/static/portfolio/JavaScript/js/main.js
@@ -33,6 +33,17 @@
     });
 
 
+    // Typed Initiate
+    if ($('.typed-text-output').length === 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
 
 
     // Modal Video
@@ -54,7 +65,7 @@
 
 
     // Scroll to Bottom
-    $(window).scroll(function () {
+    $(document).ready(function () {
         if ($(this).scrollTop() > 100) {
             $('.scroll-to-bottom').fadeOut('slow');
         } else {
